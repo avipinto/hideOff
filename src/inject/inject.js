64cf4d9df9f8600add7c7cd1820fe9d6e58ef419
avipinto <pinto.avi@gmail.com>
2013-10-17 17:24:50 +0300

    .

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,8 +7,29 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-		jQuery(document).on("mouseenter mouseleave","img",function(event){this.src = "";});
+		 jQuery(document).on("load","img",function(event)
+		 	{
+		 		//this.src = "";
+		 		$(this).css("opacity","0.2");
+		 	});
+		 
+		 // jQuery(document).on("mousedown","img",function(event)
+		 // 	{
+		 // 		//this.src = "";
+		 // 		$(this).css("opacity","0.2");
+		 // 	});
+
+		 // jQuery(document).on("mouseenter mousemove","img",function(event)
+		 // 	{
+		 // 		//this.src = "";
+		 // 		$(this).css("opacity","0.2");
+		 // 	});
+		//$("img").on("mouseenter",function(event){this.src = "";});
+		$("img").css("opacity","0.2");
 	}
 	}, 10);
+
+
 });
+
 

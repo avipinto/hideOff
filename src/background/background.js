86633fdd0daf172cea36580fd3e9530aa2ba9b49
ohadshany <ohad.shany@gmail.com>
2013-10-18 00:17:54 +0300

    Add triple state page action

--- src/background/background.js
+++ src/background/background.js
@@ -3,13 +3,34 @@
 chrome.pageAction.onClicked.addListener(function(tab)
  {
     //Do here the action
+    chrome.tabs.sendMessage(tab.id, {target: "inj", action: "paclicked"});
+    
  });
     
  
 chrome.runtime.onMessage.addListener(
 
   function(request,sender, callback) {
-    chrome.pageAction.show(sender.tab.id);
+
+    if (request.target !== "bg") {
+        return;
+    }
+    
+    var tabId = sender.tab.id;
+    
+    if (request.showpa === true) {
+        var title = "hideOff: Enable"; 
+        var iconpath = "icons\\sun19g.png";
+        if (request.hasHides === true) {
+            iconpath = "icons\\sun19.png";
+            title = "hideOff: Show All"; 
+        }
+        chrome.pageAction.setIcon({path: iconpath , tabId: tabId });
+        chrome.pageAction.setTitle({title: title, tabId: tabId });
+        chrome.pageAction.show(tabId);
+    } else {
+        chrome.pageAction.hide(tabId);    
+    }
     callback({});
   } );
  

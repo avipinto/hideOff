9dec8ce525bf080cf8088a263025b498723c133d
avipinto <pinto.avi@gmail.com>
2013-10-17 20:27:52 +0300

    show hidden

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -9,14 +9,26 @@ chrome.extension.sendMessage({}, function(response) {
 		 // 		$(this).css("opacity","0.2");
 		 // 	});
 
-		 // jQuery(document).on("mouseenter mousemove","img",function(event)
+		 // jQuery(document).on("mouseenter mousemove",".hideOff-cont",function(event)
 		 // 	{
 		 // 		//this.src = "";
-		 // 		$(this).css("opacity","0.2");
+		 // 		$(this).find(".hideOff-show").show();
 		 // 	});
 		//$("img").on("mouseenter",function(event){this.src = "";});
 		
-		$("img").addClass("hideOff-hide");
+		//  jQuery(document).on("click",".hideOff-show",function(event)
+		// {
+		// 	//this.src = "";
+		// 	alert("ddd");
+		// 	return false;
+		// });
+
+		//$("img").addClass("hideOff-hide");
+		$("img").each(function()
+			{
+				hideImage(this);
+			});
+		//hideImage($(newEl));
 		var observer = new MutationSummary({
   					callback: imagesAdded,
   					queries: [{ element: 'img' }]
@@ -27,11 +39,23 @@ chrome.extension.sendMessage({}, function(response) {
 
 });
 
-function hideImage(img)
+function hideImage(imgEl)
 {
+	img = $(imgEl);
 	var wrap = $("<div class='hideOff-cont'></div>");
+	var show = $("<div class='hideOff-show' title='Show Again'>^</div>");
+	wrap.on("mouseenter",function(){$(this).find(".hideOff-show").show()});
+	show.on("click",function()
+	{
+		$(this).siblings("img").removeClass("hideOff-hide");
+		$(this).remove();
+		return false;
+	});
+	wrap.width(img.width());
+	wrap.height(img.height());
 	img.wrap(wrap);
-	wrap.append()
+	img.after(show);
+	img.addClass("hideOff-hide");
 
 
 }
@@ -42,7 +66,8 @@ function imagesAdded(summaries)
 
 	images.added.forEach(function(newEl) 
 	{
-	$(newEl).addClass("hideOff-hide");
+		//$(newEl).addClass("hideOff-hide");
+		hideImage(newEl);
 	// do setup work on new elements with data-h-tweet
 	});
 
